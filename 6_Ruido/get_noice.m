c61f00017e16291d28bcd389379334d56d156180
Eduardo Matias Luna <30338751@fi.unju.edu.ar>
2021-05-26 15:06:23 -0300

    add missing 6d excercise

--- 6_Ruido/get_noice.m
+++ 6_Ruido/get_noice.m
@@ -3,8 +3,14 @@ function [] = get_noice()
 x_1 = linspace(0,2/3*pi,50);
 y_1 = 10 * sin(2*x_1) + 5;
 
+MEAN_1 = mean(y_1);
+STANDARD_1 = std(y_1);
+
 x_2 = linspace(0,2/3*pi,100);
 y_2 = 10 * sin(2*x_2) + 5 - 1 + 2 * rand(1,100);
+
+MEAN_2 = mean(y_2);
+STANDARD_2 = std(y_2);
 
 plot(x_1, y_1, 'g');
 hold on;
